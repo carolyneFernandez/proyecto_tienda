428ed4766dd7b5616cb828b271c3cdba0966078d
carolyne <sheresade.carolyne.nicolle.com>
2016-04-27 23:01:18 +0200

    probando

--- usuarios/index.php
+++ usuarios/index.php
@@ -4,8 +4,6 @@ include_once("../plantilla/db_configuration.php");
   include_once("../plantilla/configuration_instalacion.php");
   $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($connection->connect_errno) {
-
-  include_once("../plantilla/configuration_instalacion.php");
    printf("Connection failed: %s\n", $connection->connect_error);
    header('location: ../instalacion.php');
 
