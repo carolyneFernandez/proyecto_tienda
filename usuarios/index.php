f1ffb0da184ef52d258de696d814ad005f1bc989
carolyne <sheresade.carolyne.nicolle.com>
2016-04-27 22:58:18 +0200

    probando

--- usuarios/index.php
+++ usuarios/index.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once("../plantilla/db_configuration.php");
+  include_once("../plantilla/configuration_instalacion.php");
   $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($connection->connect_errno) {
 
